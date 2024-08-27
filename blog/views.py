038d28a59f2b88be129b101349621bd37615afcd
luisp6914 <117806005+luisp6914@users.noreply.github.com>
2024-08-26 19:16:56 -0700

    Starting a Blog app
    Starting the blog app and creating route for it

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 '''
 This is the script that handles the routes 
 '''
 # Create your views here.
 def home(request):
-    return HttpResponse(" <h1> Blog Home </h1> ")
+    return render(request, 'blog/home.html')
 
 def about(request):
-    return HttpResponse(" <h1> Blog About </h1> ")
+    return render(request, 'blog/about.html')
