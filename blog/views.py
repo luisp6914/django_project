201fc08a00ff2fbc0f49f7c17655c2bb89a6253f
luisp6914 <117806005+luisp6914@users.noreply.github.com>
2024-09-05 18:02:16 -0700

    Chaning the templates
    Breaking the code down into modules, so it can be easier to use reusable code

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,26 @@ from django.shortcuts import render
 This is the script that handles the routes 
 '''
 # Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'This is the content of the first blog post',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'This is the content of the second blog post',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title' : 'About'})
